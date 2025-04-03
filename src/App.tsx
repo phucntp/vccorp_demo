0acee02ac247506262786955f4651be91f3d220b
phucnt <phucnt@namviet-corp.com.vn>
2025-04-03 16:48:54 +0700

    fix bug app

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,41 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="relative flex items-center justify-end h-screen w-[375px] xs:w-[375px] sm:w-[640px] md:[768px] lg:w-[1024px] bg-[url('/images/Background_SM.png')] lg:bg-[url('/images/Background.png')] xs:bg-[url('/images/Background.png')] sm:bg-[url('/images/Background.png')] bg-right-top bg-cover overflow-hidden">
-      <div className="w-[260px] h-[358px] top-[-36px] left-0 p-0 xs:left-[-20px] xs:top-[118px] xs:w-[455px] xs:h-[587px] xs:left-[-20px] sm:left-[-20px] sm:top-[118px] sm:w-[324px] sm:h-[420px] sm:p-[20px] lg:left-[-20px] lg:top-[118px] lg:w-[455px] lg:h-[587px] lg:p-[20px] bg-[url('/images/border_1.png')] bg-right-top bg-cover absolute">
+    <div className="relative flex items-center justify-end h-screen w-[375px] sm:w-[640px] md:[768px] lg:w-[1024px] bg-[url('/images/Background_SM.png')] lg:bg-[url('/images/Background.png')] sm:bg-[url('/images/Background.png')] bg-right-top bg-cover overflow-hidden">
+      <div className="w-[260px] h-[358px] top-[-36px] left-0 p-0 sm:left-[-20px] sm:top-[118px] sm:w-[324px] sm:h-[420px] sm:p-[20px] lg:left-[-20px] lg:top-[118px] lg:w-[455px] lg:h-[587px] lg:p-[20px] bg-[url('/images/border_1.png')] bg-right-top bg-cover absolute">
         <img
           src="/images/doctor_2.png"
           alt="Doctor 2"
-          className="w-full h-full object-contain xs:object-cover sm:object-cover lg:object-cover ml-[-8px] mt-[1px] xs:mt-0 xs:ml-[-8px] sm:mt-0 sm:ml-[-8px] lg:mt-0 lg:ml-[-8px]"
+          className="w-full h-full object-contain sm:object-cover lg:object-cover ml-[-8px] mt-[1px] sm:mt-0 sm:ml-[-8px] lg:mt-0 lg:ml-[-8px]"
         />
       </div>
-      <div className="w-[56px] h-[63px] top-[24px] right-[95px] xs:w-[147px] xs:h-[166px] sm:w-[120px] sm:h-[135px] lg:w-[147px] lg:h-[166px] absolute xs:left-[306px] xs:top-[142px] sm:left-[217px] sm:top-[142px] lg:left-[280px] lg:top-[142px]">
+      <div className="w-[56px] h-[63px] top-[24px] right-[95px] sm:w-[120px] sm:h-[135px] lg:w-[147px] lg:h-[166px] absolute sm:left-[217px] sm:top-[142px] lg:left-[280px] lg:top-[142px]">
         <img
           src="/images/Vector.png"
           alt="Vector"
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="w-[50px] h-[50px] top-[104px] right-[-6px] xs:hidden sm:hidden absolute lg:hidden">
+      <div className="w-[50px] h-[50px] top-[104px] right-[-6px] sm:hidden absolute lg:hidden">
         <img
           src="/images/Vector_2.png"
           alt="Vector"
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="w-[226px] top-[154px] right-[70px] h-[256px] xs:w-[400px] xs:h-[453px] sm:w-[280px] sm:h-[317px] lg:w-[400px] lg:h-[453px] p-[3px] xs:p-[6px] sm:p-[6px] lg:p-[6px] bg-[url('/images/border_2.png')] bg-right-top bg-cover absolute xs:left-[140px] sm:left-[75px] sm:bottom-[128px] sm:top-auto lg:left-[140px] lg:bottom-[60px] lg:top-auto">
+      <div className="w-[226px] top-[154px] right-[70px] h-[256px] sm:w-[280px] sm:h-[317px] lg:w-[400px] lg:h-[453px] p-[3px] sm:p-[6px] lg:p-[6px] bg-[url('/images/border_2.png')] bg-right-top bg-cover absolute sm:left-[75px] sm:top-[368px] lg:left-[140px] lg:top-[480px]">
         <img
           src="/images/doctor_1.png"
           alt="Doctor 1"
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="text-center text-left absolute xs:right-[155px] sm:right-[0px] lg:right-[100px] px-0 lg:px-[56px] xs:px-[56px] sm:px-[58px] flex flex-col h-full w-full xs:w-[352px] sm:w-[352px] lg:w-[352px]">
-        <div className="absolute text-[16px] xs:text-[28px] sm:text-[20px] lg:text-[28px] text-[#fff] xs:top-[212px] sm:top-[212px] lg:top-[212px] bottom-[150px] px-[54px] xs:px-0 sm:px-[36px] lg:px-0">
+      <div className="text-center text-left absolute sm:right-[0px] lg:right-[100px] px-0 lg:px-[56px] sm:px-[58px] flex flex-col h-full w-full sm:w-[352px] lg:w-[352px]">
+        <div className="absolute text-[16px] sm:text-[20px] lg:text-[28px] text-[#fff] sm:top-[212px] lg:top-[212px] bottom-[150px] px-[54px] sm:px-[36px] lg:px-0">
           Tôi không nghĩ mình đang khác đi, bởi tôi là một người sống khá đơn
           giản. Tôi biết những người yêu mến tôi là vì sự chân thật và giản dị.
         </div>
-        <div className="absolute bottom-[50px] sm:bottom-[115px] lg:bottom-[115px] px-[54px] xs:px-0 lg:px-0">
+        <div className="absolute bottom-[50px] sm:bottom-[115px] lg:bottom-[115px] px-[54px] lg:px-0">
           <p className="text-yellow-400 text-[28px] font-bold">THU QUỲNH</p>
           <p className="text-[18px] text-[#fff]">Bác sĩ</p>
         </div>
